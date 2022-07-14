2b2194d5f56209e08f0712ff2a797e0229ade8f1
fabiola <fabiolasilvaoffice@gmail.com>
2022-07-14 14:33:05 -0400

    login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types = "cypress" />
+
+context('Funcionalidade Login', () => {
+
+    it('Deve realizar login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    
+    });
+        
+    
+    
+});
+
