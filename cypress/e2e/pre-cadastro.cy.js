3d564522ecbb88e021a674081256a4edd4d719c7
fabiola <fabiolasilvaoffice@gmail.com>
2022-07-15 15:52:15 -0400

    trazendo dados dinâmico para uma meelhor automatizacao

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -9,20 +9,19 @@ describe('Funcionalidade de Pré-Cadastro do Usuário', () => {
     
     it('Deve completar o preenchimento do pré-cadastro', () => {
 
-        const Name = faker.name.firstName()
-        const Sobrenome = faker.name.lastName()
-        const Email =faker.internet.email()
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker =faker.internet.email()
 
-        cy.get('#reg_email').type('bruno_teste@teste.com')
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@teste$')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type('Bruno')
-        cy.get('#account_last_name').type('Carvalho')
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
-
         
     });
 });
