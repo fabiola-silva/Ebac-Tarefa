ab7db1646b212413722502512769cc24fb027a5c
fabiola <fabiolasilvaoffice@gmail.com>
2022-07-15 14:14:19 -0400

    Tela de pre-cadastro

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,16 @@
+/// <reference types= "cypress" />
+
+describe('Funcionalidade de Pré-Cadastro do Usuário', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('', () => {
+        
+    });
+
+});
+
+
+
