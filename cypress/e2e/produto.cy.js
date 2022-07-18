1d83c4fa631fbf7f9f05d158321763d7264f8113
fabiola <fabiolasilvaoffice@gmail.com>
2022-07-18 17:40:11 -0400

    Adicionando produto no carrinho

--- cypress/e2e/produto.cy.js
+++ cypress/e2e/produto.cy.js
@@ -13,4 +13,18 @@ describe('Funcionalidade Página do Produto', () => {
         .click()
     });
 
+    it('Deve adicionar o produto no carrinho', () => {
+        cy.get('[class="product-block grid"]')
+        .contains('Arcadio Gym Short').click()
+        cy.get('.button-variable-item-32').click()
+        cy.get('.button-variable-item-Black').click()
+        cy.get('.input-text').clear().type('2')
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+        cy.get('.woocommerce-message').should('contain', '2 × “Arcadio Gym Short” foram adicionados no seu carrinho.')
+
+                
+    });
+
 });
